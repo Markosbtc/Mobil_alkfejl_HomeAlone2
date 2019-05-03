bad30bdfdc6786fbdacf9f44fe8fdbda4008801d
Subotic Marko <Subotic.Marko@stud.u-szeged.hu>
2019-05-03 14:02:43 +0200

    save files

--- app/src/main/java/com/example/homealone2/MainActivity.java
+++ app/src/main/java/com/example/homealone2/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.homealone2;
 
 import android.Manifest;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -9,7 +8,6 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.storage.StorageVolume;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
@@ -18,10 +16,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView mListview;
     private List<String> myList = new ArrayList<>();
     private ArrayAdapter<String> adapter;
-    private static final String FILE_TYPE_NO_MEDIA = ".nomedia";
+    private Button savebtn;
 
 
     // Requesting permission to RECORD_AUDIO
@@ -75,45 +78,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.savebtn = findViewById(R.id.button3);
         this.mListview  = findViewById(R.id.theList);
         getPermissionToRecordAudio();
-
-
+        savebtn.setClickable(false);
         AdapterSettings();
-
-
     }
 
-    public void getRecordings(){
-        ContentResolver contentResolver = getContentResolver();
-        Uri recordingUri  = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        String path = "/storage/emulated/0/MIUI/sound_recorder";
+    public void myRecordings(){
+        File root = android.os.Environment.getExternalStorageDirectory();
+        String path = root.getAbsolutePath() + "/Recordings";
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        //Log.d("Files", "Size: "+ files.length);
+        if( files!=null ){
 
-        Cursor cursor = contentResolver.query(recordingUri,null,null,null,null);
+            for (int i = 0; i < files.length; i++) {
 
-        if(cursor!=null && cursor.moveToFirst()){
-            int fileColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DATA);
-            int title = cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME);
-            int duration = cursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+                Log.d("Files", "FileName:" + files[i].getAbsolutePath());
+                String fileName = files[i].getAbsolutePath();
 
-
-            do{
-                String audioFilePath = cursor.getString(fileColumn);
-                String currentTitle = cursor.getString(title);
-                String currentDuration = cursor.getString(duration);
-
-                myList.add(audioFilePath);
-            } while (cursor.moveToNext());
-            cursor.close();
+                myList.add(fileName);
+            }
         }
     }
 
     public void AdapterSettings(){
         myList.clear();
-        getRecordings();
+        myRecordings();
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myList);
 
-        //adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,filterFiles());
 
         mListview.setAdapter(adapter);
         mListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -122,87 +117,62 @@ public class MainActivity extends AppCompatActivity {
                 String str = mListview.getItemAtPosition(position).toString();
                 //Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
                 playSelected(str);
+
             }
         });
-
-
     }
 
-
-
-
-
-    private ArrayList<String> filterFiles() {
-
-        ArrayList<String> listOfHiddenFiles = new ArrayList<>();
-        String hiddenFilePath;
-
-        // Scan all no Media files
-        String nonMediaCondition = MediaStore.Files.FileColumns.MEDIA_TYPE
-                + "=" + MediaStore.Files.FileColumns.MEDIA_TYPE_NONE;
-
-        // Files with name contain .nomedia
-        String where = nonMediaCondition + " AND "
-                + MediaStore.Files.FileColumns.TITLE + " LIKE ?";
-
-        String[] params = new String[] { "%" + FILE_TYPE_NO_MEDIA + "%" };
-
-        // make query for non media files with file title contain ".nomedia" as
-        // text on External Media URI
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(
-                MediaStore.Files.getContentUri("external"),
-                new String[] { MediaStore.Files.FileColumns.DATA }, where,
-                params, null);
-
-        // No Hidden file found
-        if (cursor.getCount() == 0) {
-
-            listOfHiddenFiles.add("No Hidden File Found");
-
-            // show Nothing Found
-            return listOfHiddenFiles;
+    void savefile(Uri sourceuri){
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File file = new File(root.getAbsolutePath() + "/Recordings");
+        if (!file.exists()) {
+            file.mkdirs();
         }
 
-        // Add Hidden file name, path and directory in file object
-        while (cursor.moveToNext()) {
-            hiddenFilePath = cursor.getString(cursor
-                    .getColumnIndex(MediaStore.Files.FileColumns.DATA));
-            if (hiddenFilePath != null) {
+        //String sourceFilename= sourceuri.getPath();
+        //String sourceFilename= String.valueOf(sourceuri);
+        String sourceFilename= getRealPathFromURI(sourceuri);
+        String destinationFilename =  root.getAbsolutePath() + "/Recordings/" + String.valueOf(System.currentTimeMillis() + ".mp3");
 
-                listOfHiddenFiles.add(hiddenFilePath.substring(0, hiddenFilePath.lastIndexOf(File.separator)));
+        Log.d("save", "savefile: "+sourceFilename);
+        Log.d("save", "savefile: dest: "+destinationFilename);
+
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            bis = new BufferedInputStream(new FileInputStream(sourceFilename));
+            bos = new BufferedOutputStream(new FileOutputStream(destinationFilename, false));
+            byte[] buf = new byte[1024];
+            bis.read(buf);
+            do {
+                bos.write(buf);
+            } while(bis.read(buf) != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null) bis.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-
-        cursor.close();
-
-        return listOfHiddenFiles;
-
     }
 
-    public boolean isDirHaveImages(String hiddenDirectoryPath) {
-
-        File listFile[] = new File(hiddenDirectoryPath).listFiles();
-
-        boolean dirHaveRecordings = false;
-
-        if (listFile != null && listFile.length > 0) {
-            for (int i = 0; i < listFile.length; i++) {
-
-                if (listFile[i].getName().endsWith(".mp3")
-                        /*|| listFile[i].getName().endsWith(".jpg")
-                        || listFile[i].getName().endsWith(".jpeg")
-                        || listFile[i].getName().endsWith(".gif")*/) {
-
-                    // Break even if folder have a single image file
-                    dirHaveRecordings = true;
-                    break;
-
-                }
+    public String getRealPathFromURI(Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Audio.Media.DATA };
+            cursor = this.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
-        return dirHaveRecordings;
-
     }
 
     public void playSelected(String path){
@@ -233,117 +203,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-  /*  private void fetchRecordings() {
-        myList.clear();
-        File root = android.os.Environment.getExternalStorageDirectory();
-
-        String path = root.getAbsolutePath() ;
-        Log.d("Files", "Path: " + path);
-        File directory = new File(path);
-        File[] files = directory.listFiles();
-        Log.d("Files", "Size: "+ files.length);
-        if( files!=null ){
-
-            for (int i = 0; i < files.length; i++) {
-
-                Log.d("Files", "FileName:" + files[i].getName());
-                String fileName = files[i].getName();
-//                String recordingUri = root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios/" + fileName;
-                String recordingUri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI + fileName;
-                myList.add(recordingUri);
-            }
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,myList);
-            mListview.setAdapter(adapter);
-
-
-        }
-    }*/
-
-  /*  private void ListDir(@org.jetbrains.annotations.NotNull File f) {
-        File[] files = f.listFiles();
-        myList.clear();
-        Log.d("EZ", "ListDir: Files.length: " + files.length);
-        for (File file: files){
-            myList.add(file.getPath());
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,myList);
-        mListview.setAdapter(adapter);
-    }*/
-
-
-
- /*   public ArrayList<File> GetFiles(File DirectoryPath) {
-        ArrayList<File> myfiles = new ArrayList<File>();
-        File[] files = DirectoryPath.listFiles();
-        for(File singlefile : files){
-            if(singlefile.isDirectory() && !singlefile.isHidden()){
-                myfiles.addAll(GetFiles(singlefile));
-
-            }
-            else {
-                if (singlefile.getName().endsWith("records.mp3")) {
-                    myfiles.add(singlefile);
-                }
-            }
-        }
-
-        return myfiles;
-    }*/
- /*
-    public void listings(){
-        final String[] items;
-        // String path=Environment.getExternalStorageDirectory();
-        final ListView lv=(ListView)findViewById(R.id.theList);
-        final ArrayList<File> myrecords = GetFiles(Environment.getExternalStorageDirectory());
-        items=new String[myrecords.size()];
-        for(int i=0;i<myrecords.size();i++){
-            items[i]=myrecords.get(i).getName();
-        }
-        final ArrayAdapter<String> adapt=new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
-        lv.setAdapter(adapt);
-//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                // String filename =(String)lv.getItemAtPosition(position);
-//
-//                String path=Environment.getExternalStorageDirectory().getAbsolutePath()
-//                        +  "/"+(String)lv.getItemAtPosition(position);
-//                Toast.makeText(getApplicationContext(),path,Toast.LENGTH_SHORT).show();
-//                Uri audio = Uri.parse(path);
-//                Intent intent = new Intent();
-//                intent.setAction(android.content.Intent.ACTION_VIEW);
-//                File file = new File(String.valueOf(audio));
-//                intent.setDataAndType(Uri.fromFile(file), "audio/*");
-//                startActivity(intent);
-//            }
-//        });
-    }
-
-*/
-
-
-
-//    // Checks if external storage is available for read and write
-//    public boolean isExternalStorageWritable() {
-//        String state = Environment.getExternalStorageState();
-//        if (Environment.MEDIA_MOUNTED.equals(state)) {
-//            return true;
-//        }
-//        return false;
-//    }
-
-
     public void startRecording(View view) {
         Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
         startActivityForResult(intent, ACTIVITY_RECORD_SOUND);
-        //  /storage/emulated/0/MIUI/sound_recorder/
 
     }
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -357,67 +221,17 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("EZ", "getcontenturiforpath: " +
                         MediaStore.Audio.Media.getContentUriForPath(audioFileUri.getPath()));
                 //getcontenturiforpath: content://media/internal/audio/media
+
+                savebtn.setClickable(true);
             }
         }
     }
-/*
-    public void saveRec(View view){
-        if (this.audioFileUri != null){
-            //savefile(this.audioFileUri);
-            File as = new File(this.audioFileUri.getPath());
-            File newfile = new File(this.storageDir,as.toString());
-            newfile.mkdirs();
-            settingAdapter();
-        }
-    }
-    public File getPublicAlbumStorageDir(String albumName) {
-        // Get the directory for the user's public pictures directory.
-        File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), albumName);
-        if (!file.mkdirs()) {
-            Log.e("FILE", "Directory not created");
-        }
-        return file;
-    }
-*/
-/*
-    private void savefile(Uri sourceuri) {
-        String sourceFilename= sourceuri.getPath();
-        String destinationFilename = android.os.Environment.getExternalStorageDirectory().getPath()+File.separatorChar+"abc.mp3";
 
-        BufferedInputStream bis = null;
-        BufferedOutputStream bos = null;
-
-        try {
-            bis = new BufferedInputStream(new FileInputStream(sourceFilename));
-            bos = new BufferedOutputStream(new FileOutputStream(destinationFilename, false));
-            byte[] buf = new byte[1024];
-            bis.read(buf);
-            do {
-                bos.write(buf);
-            } while(bis.read(buf) != -1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (bis != null) bis.close();
-                if (bos != null) bos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        /*InputStream in =  getContentResolver().openInputStream("your_uri_here");
-        OutputStream out = new FileOutputStream(new File("your_file_here"));
-        byte[] buf = new byte[1024];
-        int len;
-        while((len=in.read(buf))>0){
-            out.write(buf,0,len);
-        }
-        out.close();
-        in.close();
+    public void saveRecording(View view){
+        savefile(this.audioFileUri);
+        AdapterSettings();
+        savebtn.setClickable(false);
     }
-*/
 
     public void playRec(View view){
         float speed = 0.5f;
@@ -425,9 +239,8 @@ public class MainActivity extends AppCompatActivity {
             player = new MediaPlayer();
             try {
                 player.setDataSource(this,audioFileUri);
-                // TODO  player.setDataSource(listItem.getFilePath());
                 player.prepare();
-               // player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
+                // player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
                 //Toast.makeText(this,"set playback params", Toast.LENGTH_SHORT).show();
                 //player.start();
 
